69e9cd4ea5a51b77e112ca803c28c0389f625991
joshbae119 <mclceo119@gmail.com>
2025-01-17 09:24:26 +0900

    Add navigation button to modify todo item in ReadPage component

--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,11 +1,22 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { useCallback } from "react";
 
 const ReadPage = () => {
     const { tno } = useParams();
+    
+    const navigate = useNavigate();
+
+    const MoveToModify = useCallback(  (tno) => {
+        navigate({pathname:`/todo/modify/${tno}`})
+    } , [tno])
+    
 
     return (
         <div className="text-3xl font-extrabold">
             글읽기 컴포넌트 {tno}
+            <div>
+                <button onClick={() => MoveToModify(33)}>수정하기</button>
+            </div>
         </div>
     )
 }
